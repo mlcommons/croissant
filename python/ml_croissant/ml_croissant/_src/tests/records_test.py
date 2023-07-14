373a3b236c211b555d1323a2f8adcdb5a8399479
Pierre Marcenac <pierremarcenac@google.com>
2023-07-14 09:26:56 +0000

    Human-readable pkl files in tests

---
+++ python/ml_croissant/ml_croissant/_src/tests/records_test.py
@@ -0,0 +1,19 @@
+"""records_test module."""
+
+from ml_croissant._src.tests.records import record_to_python
+import pandas as pd
+
+
+def test_record_to_python():
+    assert record_to_python(
+        {
+            "key1": 1,
+            "key2": {
+                "key3": pd.Timestamp("2017-01-01T12"),
+                "key4": {"key5": b"foo", "key6": float("nan")},
+            },
+        }
+    ) == {
+        "key1": 1,
+        "key2": {"key3": "2017-01-01 12:00:00", "key4": {"key5": "Zm9v", "key6": None}},
+    }
