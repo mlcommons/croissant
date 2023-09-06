46baf9c2a32190a21e551017ad393883ce54b99d
Pierre Marcenac <pierremarcenac@google.com>
2023-09-06 14:26:26 +0000

    Remove tracing back the package name in mlcroissant

--- python/ml_croissant/ml_croissant/_src/core/optional_test.py
+++ python/ml_croissant/ml_croissant/_src/core/optional_test.py
@@ -7,11 +7,6 @@ import pytest
 
 from ml_croissant._src.core.optional import _try_import
 from ml_croissant._src.core.optional import deps
-from ml_croissant._src.core.optional import list_optional_deps
-
-
-def test_list_optional_deps():
-    assert list(list_optional_deps().keys()) == ["dev", "git", "image", "parquet"]
 
 
 @pytest.mark.parametrize(
@@ -26,9 +21,7 @@ def test_load_optional_deps(optional_deps):
 
 def test_explicit_error_message():
     with mock.patch.object(importlib, "import_module", side_effect=ImportError):
-        with pytest.raises(ImportError, match="pip install ml_croissant\\[parquet\\]"):
-            _try_import("pyarrow")
         with pytest.raises(ImportError, match="pip install foo"):
             _try_import("foo")
-        with pytest.raises(ImportError, match="pip install ml_croissant\\[git\\]"):
-            _try_import("git", package_name="GitPython")
+        with pytest.raises(ImportError, match="pip install bar"):
+            _try_import("foo", package_name="bar")
