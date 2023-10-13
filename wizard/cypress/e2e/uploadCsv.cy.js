64fc94ec6a68dd422651d99e04f09e025b34d6c6
Aida Zolic <aidazolic@google.com>
2023-10-13 14:47:41 +0200

    Use tabs for create/load dataset steps

--- wizard/cypress/e2e/uploadCsv.cy.js
+++ wizard/cypress/e2e/uploadCsv.cy.js
@@ -3,14 +3,18 @@
 import 'cypress-file-upload';
 
 describe('Wizard from local CSV', () => {
-  it('should display the form: Metadata, Files, & Record Sets', () => {
+  it('should display the form: Overview, Metadata, Resources, & Record Sets', () => {
     // Streamlit starts on :8501.
     cy.visit('http://localhost:8501')
     cy.get('button').contains('Create').click()
 
+    cy.get('[data-testid="stMarkdownContainer"]')
+      .contains('Metadata')
+      .click()
     cy.get('input[aria-label="Name:red[*]"]').type('MyDataset').blur()
-    cy.get('input[aria-label="URL:red[*]"]').type('https://mydataset.com').blur()
-    cy.get('[data-testid="stMarkdownContainer"]').contains('Files').click()
+    cy.get('input[aria-label="URL:red[*]"]').type('https://mydataset.com', {force: true})
+
+    cy.get('[data-testid="stMarkdownContainer"]').contains('Resources').click()
     // Drag and drop mimicking: streamlit/e2e/specs/st_file_uploader.spec.js.
     cy.fixture('base.csv').then((fileContent) => {
       const file = {
