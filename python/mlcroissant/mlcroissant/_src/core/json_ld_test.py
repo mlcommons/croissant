51c2fe3159edb62fc4b30c9ad32482c9ef44ecd8
ccl-core <91942859+ccl-core@users.noreply.github.com>
2023-10-19 14:43:05 +0200

    Update json_ld_test.py
    Changes in testing

--- python/mlcroissant/mlcroissant/_src/core/json_ld_test.py
+++ python/mlcroissant/mlcroissant/_src/core/json_ld_test.py
@@ -9,22 +9,18 @@ from mlcroissant._src.core.json_ld import compact_jsonld
 from mlcroissant._src.core.json_ld import expand_jsonld
 from mlcroissant._src.core.json_ld import make_context
 
-_DATASETS_FOLDER = (
-    epath.Path(__file__).parent.parent.parent.parent.parent.parent / "datasets"
-)
-_JSON_LD_PATHS = [path for path in _DATASETS_FOLDER.glob("*/*.json")]
-
 
 # If this test fails, you probably manually updated a dataset in datasets/.
 # Please, use scripts/migrations/migrate.py to migrate datasets.
-@pytest.mark.parametrize(
-    ["path"],
-    [[path] for path in _JSON_LD_PATHS],
-)
-def test_expand_and_reduce_json_ld(path):
-    with path.open() as f:
-        json_ld = json.load(f)
-    assert compact_jsonld(expand_jsonld(json_ld)) == json_ld
+def test_expand_and_reduce_json_ld():
+    dataset_folder = (
+        epath.Path(__file__).parent.parent.parent.parent.parent.parent / "datasets"
+    )
+    json_ld_paths = [path for path in dataset_folder.glob("*/*.json")]
+    for path in json_ld_paths:
+        with path.open() as f:
+            json_ld = json.load(f)
+        assert compact_jsonld(expand_jsonld(json_ld)) == json_ld
 
 
 def test_make_context():
