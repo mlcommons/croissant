866297174573a2ddd53655a3c2a3332d99d8c6b0
Clifton Tyler <ct12067@gmail.com>
2023-11-07 09:26:33 -0600

    fixing tests

---
+++ wizard/cypress/e2e/loadCroissant.cy.js
@@ -0,0 +1,29 @@
+
+describe('Wizard loads Croissant without Error', () => {
+  it('should allow uploading existing croissant files', () => {
+    cy.visit('http://localhost:8501')
+    cy.get('button').contains('Load').click()
+
+    cy.fixture('titanic.json').then((fileContent) => {
+      const file = {
+        fileContent,
+        fileName: 'titanic.json', mimeType: 'text/json',
+      }
+      cy.get(
+        "[data-testid='stFileUploadDropzone']",
+      ).attachFile(file, {
+        force: true,
+        subjectType: "drag-n-drop",
+        events: ["dragenter", "drop"],
+      })
+    })
+    cy.get('[data-testid="stExpander"]')
+      .contains('Metadata')
+      .contains('[data-testid="element-container"]')
+      .contains('Name')
+      .contains('Titanic')
+      .should('exist')
+    
+    cy.get('[data-testid="stException"]').should('not.exist')
+  })
+})
