634c20102dacc130f37f05ee9bc6b0a9e7ee1cc1
Aida Zolić <aidazolic@gmail.com>
2023-11-10 20:11:16 +0100

    Use tabs for create/load dataset steps (#328)
    Replaces expanders with tabs on the create new / load existing
    configuration page.

--- wizard/cypress/e2e/loadCroissant.cy.js
+++ wizard/cypress/e2e/loadCroissant.cy.js
@@ -20,9 +20,9 @@ describe('Wizard loads Croissant without Error', () => {
         events: ["dragenter", "drop"],
       })
     })
-    cy.get('[data-testid="stExpander"]')
-      .contains('Titanic')
-      .should('exist')
+
+    cy.get('[data-testid="stMarkdownContainer"]').contains('Metadata').click()
+    cy.get('input[aria-label="Name:red[*]"]').should('have.value', 'Titanic')
     
     cy.get('[data-testid="stException"]').should('not.exist')
   })
--- wizard/views/wizard.py
+++ wizard/views/wizard.py
@@ -3,13 +3,23 @@ import streamlit as st
 from core.state import CurrentStep
 from views.files import render_files
 from views.metadata import render_metadata
+from views.overview import render_overview
 from views.record_sets import render_record_sets
+
+OVERVIEW = "Overview"
+METADATA = "Metadata"
+RESOURCES = "Resources"
+RECORD_SETS = "Record sets"
 
 
 def render_wizard():
-    with st.expander("Metadata", expanded=True):
+    tab1, tab2, tab3, tab4 = st.tabs([OVERVIEW, METADATA, RESOURCES, RECORD_SETS])
+
+    with tab1:
+        render_overview()
+    with tab2:
         render_metadata()
-    with st.expander("Files"):
+    with tab3:
         render_files()
-    with st.expander("Record Sets"):
+    with tab4:
         render_record_sets()
