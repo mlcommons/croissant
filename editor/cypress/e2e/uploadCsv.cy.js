0c78f1589bb13cfd7f145468594e98833c7ca1ce
Pierre Marcenac <pierremarcenac@google.com>
2023-11-14 14:56:57 +0000

    Keep the canonical name (`RecordSet`, rather than natural language `Record sets`)

--- editor/cypress/e2e/uploadCsv.cy.js
+++ editor/cypress/e2e/uploadCsv.cy.js
@@ -43,7 +43,7 @@ describe('Editor loads a local CSV as a resource', () => {
     cy.contains('First rows of data:')
 
     // On the record set page, we see the record set.
-    cy.get('[data-testid="stMarkdownContainer"]').contains('Record sets').click()
+    cy.get('[data-testid="stMarkdownContainer"]').contains('RecordSets').click()
     cy.contains('base.csv_record_set (2 fields)').click()
     // We also see the fields with the proper types.
     cy.get('[data-testid="stDataFrameResizable"]').contains("column1")
