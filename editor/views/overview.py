a11aa496a9edab1914427ded8e4f35b65dacc0c1
Clifton Tyler <ct12067@gmail.com>
2023-11-14 08:22:50 -0600

    switching back to markdown as that auto-linkifies

--- editor/views/overview.py
+++ editor/views/overview.py
@@ -8,8 +8,8 @@ from utils import set_form_step
 def render_overview():
     meta = st.session_state[Metadata]
     st.subheader("Name:")
-    st.text(meta.name)
+    st.markdown(meta.name)
     st.subheader("Description:")
-    st.text(meta.description)
+    st.markdown(meta.description)
     st.subheader(f"{len(meta.distribution)} Files")
     st.subheader(f"{len(meta.record_sets)} Record Sets")
