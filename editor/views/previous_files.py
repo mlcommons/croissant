77a1ba253633ee9b0e09d4e51d9d350644ab1031
Pierre Marcenac <pierremarcenac@google.com>
2023-11-27 13:22:33 +0000

    List no project if no project is found.

--- editor/views/previous_files.py
+++ editor/views/previous_files.py
@@ -24,29 +24,30 @@ def _remove_croissant(path: epath.Path) -> None:
 
 def render_previous_files():
     paths = load_past_projects_paths()
-    if not paths:
+    has_no_project = True
+    for index, path in enumerate(paths):
+        try:
+            metadata = open_project(path)
+            timestamp = datetime.datetime.strptime(
+                path.name, PROJECT_FOLDER_PATTERN
+            ).strftime("%Y/%m/%d %H:%M")
+            label = f"{metadata.name or 'Unnamed dataset'} - {timestamp}"
+            col1, col2 = st.columns([10, 1])
+            col1.button(
+                label,
+                key=f"splash-{index}-load",
+                on_click=_load_croissant,
+                args=(metadata, path),
+            )
+            col2.button(
+                "✖️",
+                help="Warning: this will delete the project.",
+                key=f"splash-{index}-remove",
+                on_click=_remove_croissant,
+                args=(path,),
+            )
+            has_no_project = False
+        except:
+            pass
+    if has_no_project:
         st.write("No past project to load. Create one on the left!")
-    else:
-        for index, path in enumerate(paths):
-            try:
-                metadata = open_project(path)
-                timestamp = datetime.datetime.strptime(
-                    path.name, PROJECT_FOLDER_PATTERN
-                ).strftime("%Y/%m/%d %H:%M")
-                label = f"{metadata.name or 'Unnamed dataset'} - {timestamp}"
-                col1, col2 = st.columns([10, 1])
-                col1.button(
-                    label,
-                    key=f"splash-{index}-load",
-                    on_click=_load_croissant,
-                    args=(metadata, path),
-                )
-                col2.button(
-                    "✖️",
-                    help="Warning: this will delete the project.",
-                    key=f"splash-{index}-remove",
-                    on_click=_remove_croissant,
-                    args=(path,),
-                )
-            except:
-                pass
