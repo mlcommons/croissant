90ef6dfc99da10e6a0caed416f793a9eef4bb8ef
Pierre Marcenac <pierremarcenac@google.com>
2023-12-04 18:34:03 +0000

    Substitute special characters in name string.

--- editor/core/names_test.py
+++ editor/core/names_test.py
@@ -5,6 +5,7 @@ from .names import find_unique_name
 
 def test_find_unique_name():
     names = set(["first", "second", "first_0"])
+    assert find_unique_name(names, "are there spaces") == "are_there_spaces"
     assert find_unique_name(names, "first") == "first_0_0"
     assert find_unique_name(names, "second") == "second_0"
     assert find_unique_name(names, "third") == "third"
