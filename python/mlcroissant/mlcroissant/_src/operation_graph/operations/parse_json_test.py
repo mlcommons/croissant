aaed52cb8996d34415058d9abb6f43f773998156
ccl-core <91942859+ccl-core@users.noreply.github.com>
2024-01-22 15:31:24 +0000

    Define multiple record sets deriving from the same node

--- python/mlcroissant/mlcroissant/_src/operation_graph/operations/parse_json_test.py
+++ python/mlcroissant/mlcroissant/_src/operation_graph/operations/parse_json_test.py
@@ -10,10 +10,12 @@ from mlcroissant._src.tests.nodes import create_test_field
 
 def test_parse_json():
     field1 = create_test_field(
-        source=Source(extract=Extract(json_path="$.annotations[*].id"))
+        source=Source(extract=Extract(json_path="$.annotations[*].id")),
+        name="record_set_a/field1",
     )
     field2 = create_test_field(
-        source=Source(extract=Extract(json_path="$.annotations[*].value"))
+        source=Source(extract=Extract(json_path="$.annotations[*].value")),
+        name="record_set_b/field2",
     )
     fields = (field1, field2)
     json = {
@@ -23,7 +25,15 @@ def test_parse_json():
             {"id": 2, "value": 4},
         ],
     }
+
+    # Without specifying a record_set name.
     expected_df = pd.DataFrame(
         data={"$.annotations[*].id": [1, 2], "$.annotations[*].value": [3, 4]}
     )
     pd.testing.assert_frame_equal(parse_json_content(json, fields), expected_df)
+
+    # Specifying a record_set name.
+    expected_df = pd.DataFrame(data={"$.annotations[*].id": [1, 2]})
+    pd.testing.assert_frame_equal(
+        parse_json_content(json, fields, record_set="record_set_a"), expected_df
+    )
