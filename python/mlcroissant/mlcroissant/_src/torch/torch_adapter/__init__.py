3e56e4327a8a57c557f34877cdadefb7675e1329
Pierre Marcenac <marcenacp@gmail.com>
2024-01-24 13:13:36 +0100

    Break circular dependency between mlc.torch and mlc. (#461)

--- python/mlcroissant/mlcroissant/_src/torch/torch_adapter/__init__.py
+++ python/mlcroissant/mlcroissant/_src/torch/torch_adapter/__init__.py
@@ -1,6 +1,6 @@
 """PyTorch dataloader-based public API."""
 
-from .dataloader import LoaderFactory
-from .dataloader import LoaderSpecificationDataType
+from mlcroissant._src.torch.torch_adapter.dataloader import LoaderFactory
+from mlcroissant._src.torch.torch_adapter.dataloader import LoaderSpecificationDataType
 
 __all__ = ["LoaderFactory", "LoaderSpecificationDataType"]
