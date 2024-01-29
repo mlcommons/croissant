e2b322a0d024b39d1f30bb40ff7765d09e4cb1ec
Pierre Marcenac <marcenacp@gmail.com>
2024-01-29 11:32:39 +0100

    Bump version to 0.8.0. (#468)

--- editor/cypress.config.js
+++ editor/cypress.config.js
@@ -3,6 +3,6 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   // To access content within Streamlit iframes for custom components:
   chromeWebSecurity: false,
-  defaultCommandTimeout: 10000,
+  defaultCommandTimeout: 20000,
   e2e: {},
 });
