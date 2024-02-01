0287b338a6ed56387df4c6c1b3cca1326200205f
goeffthomas <goeffthomas@gmail.com>
2024-02-01 14:32:45 -0800

    Support base64 hash checking (#472)
    Added the `jsonl` output to support the Kaggle base64 example even
    though it can only be run with the correct environment variables
    configured. I have that setup locally and the tests pass after the
    change to `download.py`. If the preference is to remove the commented
    out test case, I can do that. We could setup a robot account for use in
    the GH actions/CI tests, but the tests would still break for developers
    unless they setup their Kaggle creds locally.
    
    Also include a drive-by fix as the logs from the failed tests (while I
    was developing) printed commands before we accounted for a parameterized
    `version`.
    
    Addresses #471

--- python/mlcroissant/mlcroissant/_src/operation_graph/operations/download.py
+++ python/mlcroissant/mlcroissant/_src/operation_graph/operations/download.py
@@ -1,5 +1,6 @@
 """Download operation module."""
 
+import base64
 import dataclasses
 import hashlib
 import logging
@@ -152,21 +153,29 @@ class Download(Operation):
         with filepath.open("rb") as f:
             while chunk := f.read(_CHUNK_SIZE):
                 hash.update(chunk)
-        actual_hash = hash.hexdigest()
+        # The hash from the Croissant JSON-LD can be in either hex or base64,
+        # so let's check both
         expected_hash = getattr(self.node, hash.name)
-        if actual_hash != expected_hash:
-            logging.info(
-                "Hash of downloaded file is not identical with"
-                " reference in metadata.json"
+        # First, try hex as that's likely more common
+        hex_hash = hash.hexdigest()
+        if hex_hash == expected_hash:
+            return
+        # Next, try base64 as a fallback
+        base64_hash = base64.b64encode(bytes.fromhex(hex_hash)).decode()
+        if base64_hash == expected_hash:
+            return
+
+        logging.info(
+            "Hash of downloaded file is not identical with reference in metadata.json"
+        )
+        # In v0.8 only, hashes were not checked.
+        ctx = self.node.ctx
+        if ctx.conforms_to and ctx.conforms_to > CroissantVersion.V_0_8:
+            raise ValueError(
+                f"Hash of downloaded file {filepath} is not identical with the"
+                f" reference in the Croissant JSON-LD. Expected: {expected_hash} -"
+                f" Got: {hex_hash} (hex) / {base64_hash} (base64)"
             )
-            # In v0.8 only, hashes were not checked.
-            ctx = self.node.ctx
-            if ctx.conforms_to and ctx.conforms_to > CroissantVersion.V_0_8:
-                raise ValueError(
-                    f"Hash of downloaded file {filepath} is not identical with the"
-                    f" reference in the Croissant JSON-LD. Expected: {expected_hash} -"
-                    f" Got: {actual_hash}"
-                )
 
     def _download_from_http(self, filepath: epath.Path):
         content_url = self.node.content_url
