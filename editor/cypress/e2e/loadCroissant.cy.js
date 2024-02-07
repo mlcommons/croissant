86c2b23b9cdcea36d64aaffe75882360eb4c2271
Pierre Marcenac <pierremarcenac@google.com>
2024-02-07 09:39:22 +0000

    Format with Prettier

--- editor/cypress/e2e/loadCroissant.cy.js
+++ editor/cypress/e2e/loadCroissant.cy.js
@@ -1,66 +1,62 @@
 /// <reference types="cypress" />
 
-import 'cypress-file-upload';
-import 'cypress-iframe';
-import * as path from 'path';
+import "cypress-file-upload";
+import "cypress-iframe";
+import * as path from "path";
 
-import {VERSIONS} from '../support/constants';
+import { VERSIONS } from "../support/constants";
 
-VERSIONS.forEach(version => {
-  const fixture = `${version}/titanic.json`
+VERSIONS.forEach((version) => {
+  const fixture = `${version}/titanic.json`;
   describe(`[Version ${version}] Editor loads Croissant without Error`, () => {
-    it('should allow uploading existing croissant files', () => {
-      cy.visit('http://localhost:8501')
-      cy.fixture(fixture).then((fileContent) => {
-        const file = {
-          fileContent,
-          fileName: fixture, mimeType: 'text/json',
-        }
-        cy.get(
-          "[data-testid='stFileUploadDropzone']",
-        ).attachFile(file, {
-          force: true,
-          subjectType: "drag-n-drop",
-          events: ["dragenter", "drop"],
-        })
-      })
-      cy.enter('[title="components.tabs.tabs_component"]').then(getBody => {
-        getBody().contains('Metadata').click()
-      })
-      cy
-      .get("[data-testid='element-container']")
-      .contains('Titanic')
-      .should('exist')
-    })
-    it('should download as json', () => {
-      cy.visit('http://localhost:8501')
+    it("should allow uploading existing croissant files", () => {
+      cy.visit("http://localhost:8501");
       cy.fixture(fixture).then((fileContent) => {
         const file = {
           fileContent,
           fileName: fixture,
-          mimeType: 'text/json',
-        }
-        cy.get(
-          "[data-testid='stFileUploadDropzone']",
-        ).attachFile(file, {
+          mimeType: "text/json",
+        };
+        cy.get("[data-testid='stFileUploadDropzone']").attachFile(file, {
           force: true,
           subjectType: "drag-n-drop",
           events: ["dragenter", "drop"],
-        })
-      })
-      cy.get('[data-testid="stException"]').should('not.exist')
-      cy.enter('[title="components.tabs.tabs_component"]').then(getBody => {
-        getBody().contains('Export').click()
-      })
+        });
+      });
+      cy.enter('[title="components.tabs.tabs_component"]').then((getBody) => {
+        getBody().contains("Metadata").click();
+      });
+      cy.get("[data-testid='element-container']")
+        .contains("Titanic")
+        .should("exist");
+    });
+    it("should download as json", () => {
+      cy.visit("http://localhost:8501");
+      cy.fixture(fixture).then((fileContent) => {
+        const file = {
+          fileContent,
+          fileName: fixture,
+          mimeType: "text/json",
+        };
+        cy.get("[data-testid='stFileUploadDropzone']").attachFile(file, {
+          force: true,
+          subjectType: "drag-n-drop",
+          events: ["dragenter", "drop"],
+        });
+      });
+      cy.get('[data-testid="stException"]').should("not.exist");
+      cy.enter('[title="components.tabs.tabs_component"]').then((getBody) => {
+        getBody().contains("Export").click();
+      });
       cy.fixture(fixture).then((fileContent) => {
         const downloadsFolder = Cypress.config("downloadsFolder");
         cy.readFile(path.join(downloadsFolder, "croissant-titanic.json"))
-        .then((downloadedFile) => {
-          downloadedFile = JSON.stringify(downloadedFile)
-          return downloadedFile
-        })
-        .should('deep.equal', JSON.stringify(fileContent))
-      })
-    })
-  })
-})
+          .then((downloadedFile) => {
+            downloadedFile = JSON.stringify(downloadedFile);
+            return downloadedFile;
+          })
+          .should("deep.equal", JSON.stringify(fileContent));
+      });
+    });
+  });
+});
