75683eced5637dbf69e7cf79705c623c7b63b934
Aida Zolić <aidazolic@gmail.com>
2024-02-09 14:00:38 +0100

    Fix query params (#505)
    `query_params` is not a `dict` so usage is a bit different from the
    return values of `st.experimental_get_query_params`, see
    [documentation](https://docs.streamlit.io/library/api-reference/utilities/st.query_params)

--- editor/core/query_params.py
+++ editor/core/query_params.py
@@ -15,28 +15,24 @@ class QueryParams:
     OPEN_RECORD_SET = "recordSet"
 
 
-def _get_query_param(params: dict[str, Any], name: str) -> str | None:
+def _get_query_param(name: str) -> str | None:
     """Gets query param with the name `name`."""
-    if name in params:
-        param = params[name]
-        if isinstance(param, list) and len(param) > 0:
-            return param[0]
+    param = st.query_params.get_all(name)
+    if isinstance(param, list) and len(param) > 0:
+        return param[0]
     return None
 
 
 def _set_query_param(param: str, new_value: str) -> str | None:
     params = st.query_params
-    if params.get(param) == [new_value]:
+    if params.get_all(param) == [new_value]:
         # The value already exists in the query params.
         return
-    new_params = {k: v for k, v in params.items() if k != param}
-    new_params[param] = new_value
-    st.query_params = new_params
+    params[param] = new_value
 
 
 def is_record_set_expanded(record_set: RecordSet) -> bool:
-    params = st.query_params
-    open_record_set_name = _get_query_param(params, QueryParams.OPEN_RECORD_SET)
+    open_record_set_name = _get_query_param(QueryParams.OPEN_RECORD_SET)
     if open_record_set_name:
         return open_record_set_name == record_set.name
     return False
@@ -47,8 +43,7 @@ def expand_record_set(record_set: RecordSet) -> None:
 
 
 def get_project_timestamp() -> str | None:
-    params = st.query_params
-    return _get_query_param(params, QueryParams.OPEN_PROJECT)
+    return _get_query_param(QueryParams.OPEN_PROJECT)
 
 
 def set_project(project: CurrentProject):
