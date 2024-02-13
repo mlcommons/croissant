ad7d8c4df6390d643dbb6476aebc355db792f77b
ccl-core <91942859+ccl-core@users.noreply.github.com>
2024-02-13 17:12:11 +0100

    Update postCreateCommand.sh

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -3,19 +3,5 @@
 sudo apt-get update && sudo apt-get install -y libgraphviz-dev git-lfs
 
 # Install dependencies except mlcroissant itself
-pip install absl-py \
-          datasets \
-          etils[epath]@git+https://github.com/google/etils.git#egg=etils[epath] \
-          GitPython \
-          jsonpath_rw \
-          mypy \
-          networkx \
-          pandas \
-          Pillow \
-          pyarrow \
-          pytest \
-          pytype \
-          rdflib \
-          requests \
-          torchdata \
-          tqdm
+cd ../python/mlcroissant
+pip install -e ../python/mlcroissant/.[dev]
