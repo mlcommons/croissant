3c0042e7ce949d677ec291ad32ecd34e4f2d13d7
ccl-core <91942859+ccl-core@users.noreply.github.com>
2024-02-14 10:40:44 +0100

    [postCreateCommand] Update apt-get before running apt-get install (#509)

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,21 +1,7 @@
 #!/bin/bash
 
-sudo apt-get install -y libgraphviz-dev git-lfs
+sudo apt-get update && sudo apt-get install -y libgraphviz-dev git-lfs
 
 # Install dependencies except mlcroissant itself
-pip install absl-py \
-          datasets \
-          etils[epath] \
-          GitPython \
-          jsonpath_rw \
-          mypy \
-          networkx \
-          pandas \
-          Pillow \
-          pyarrow \
-          pytest \
-          pytype \
-          rdflib \
-          requests \
-          torchdata \
-          tqdm
+cd ../python/mlcroissant
+pip install -e ../python/mlcroissant/.[dev]
