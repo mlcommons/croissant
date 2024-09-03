d1193d4ef6e275fa70298e84455eeeb1e5efb35e
ccl-core <91942859+ccl-core@users.noreply.github.com>
2024-09-03 11:18:09 +0200

    Adding the levanti dataset. (#727)

--- python/mlcroissant/mlcroissant/_src/datasets_test.py
+++ python/mlcroissant/mlcroissant/_src/datasets_test.py
@@ -195,6 +195,7 @@ def test_nonhermetic_loading(version, dataset_name, record_set_name, num_records
     [
         ["huggingface-anthropic-hh-rlhf/metadata.json", "red-team-attempts", 10, None],
         ["huggingface-c4/metadata.json", "data", 1, {"variant": "en"}],
+        ["huggingface-levanti/metadata.json", "levanti_train", 10, None],
     ],
 )
 def test_nonhermetic_loading_1_0(dataset_name, record_set_name, num_records, filters):
