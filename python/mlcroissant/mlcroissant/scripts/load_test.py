a23b44c4ea1755cac5506cdb58c000d7f2036fa3
ccl-core <91942859+ccl-core@users.noreply.github.com>
2024-09-03 14:09:46 +0200

    Add splits to the huggingface-mnist dataset (#726)

--- python/mlcroissant/mlcroissant/scripts/load_test.py
+++ python/mlcroissant/mlcroissant/scripts/load_test.py
@@ -7,18 +7,18 @@ from mlcroissant._src.tests.versions import parametrize_version
 from mlcroissant.scripts import load as load_lib
 
 
-@parametrize_version()
-def test_should_raise_when_no_record_set(version):
+def test_should_raise_when_no_record_set():
     dataset_name = "huggingface-mnist"
     file = (
         epath.Path(__file__).parent.parent.parent.parent.parent
         / "datasets"
-        / version
+        / "1.0"
         / dataset_name
         / "metadata.json"
     )
     with pytest.raises(
-        ValueError, match="--record_set flag should have a value in `default`"
+        ValueError,
+        match="--record_set flag should have a value in `mnist_splits`, `default`",
     ):
         load_lib.load(jsonld=file, record_set=None)
 
