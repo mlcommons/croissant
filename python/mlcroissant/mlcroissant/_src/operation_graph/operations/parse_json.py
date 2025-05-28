33f374569d3c951d6465e89340a894fbb31b0343
Cristina E. Gonzalez-Espinoza <crisbeth46@gmail.com>
2025-05-28 11:49:23 +0200

    add reading option for fhir

--- python/mlcroissant/mlcroissant/_src/operation_graph/operations/parse_json.py
+++ python/mlcroissant/mlcroissant/_src/operation_graph/operations/parse_json.py
@@ -9,12 +9,18 @@ from mlcroissant._src.structure_graph.nodes.field import Field
 
 def parse_json_content(json: Json, fields: tuple[Field, ...]) -> pd.DataFrame:
     """Parsed all JSONs defined in the fields of RecordSet and outputs a pd.DF."""
-    series = {}
-    for field in fields:
-        json_path = field.source.extract.json_path
-        if json_path is None:
-            continue
-        jsonpath_expression = jsonpath_rw.parse(json_path)
-        values = [match.value for match in jsonpath_expression.find(json)]
-        series[json_path] = values
-    return pd.DataFrame(series)
+    lines = json if isinstance(json, list) else [json]
+    rows = []
+    for line in lines:
+        row = {}
+        for field in fields:
+            path = field.source.extract.json_path
+            if not path:
+                continue
+            expr = jsonpath_rw.parse(path)
+            matches = expr.find(line)
+            if matches:
+                # if you expect a single value, grab the first
+                row[path] = matches[0].value
+        rows.append(row)
+    return pd.DataFrame(rows)
