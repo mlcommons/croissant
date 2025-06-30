mlcommons/croissant

commit ea7eff778791a0a476aba42db0cda86927b4b35e

Allow to specify multiple conformsTo (#892)
Addresses #824
https://github.com/mlcommons/croissant/issues/824#issuecomment-2915373094

python/mlcroissant/mlcroissant/_src/structure_graph/nodes/metadata.py
@@ -51,6 +51,17 @@ class Metadata(Node):
         input_types=[SDO.Text],
         url=constants.ML_COMMONS_CITE_AS,
     )
+    conforms_to: list[str] = mlc_dataclasses.jsonld_field(
+        cardinality="MANY",
+        default_factory=list,
+        description=(
+            "The specifications the dataset conforms to. mlcroissant will use the first"
+            " valid CroissantVersion for validation. Note that, to use mlcroissant, at"
+            " least one valid CroissantVersion should be provided."
+        ),
+        input_types=[SDO.Text],
+        url=constants.DCTERMS_CONFORMS_TO,
+    )
     creators: list[Organization | Person] = mlc_dataclasses.jsonld_field(
         cardinality="MANY",
         default_factory=list,
@@ -355,7 +366,12 @@ class Metadata(Node):
         jsonld = super().to_json()
         jsonld.pop("@id", None)
         jsonld["@context"] = context
-        conforms_to = self.ctx.conforms_to.to_json() if self.ctx.conforms_to else None
+        if self.conforms_to:
+            conforms_to = (
+                self.conforms_to[0] if len(self.conforms_to) == 1 else self.conforms_to
+            )
+        else:
+            conforms_to = None
         jsonld["conformsTo"] = conforms_to
         if self.ctx.is_live_dataset:
             jsonld["isLiveDataset"] = self.ctx.is_live_dataset

python/mlcroissant/mlcroissant/_src/core/context.py
@@ -12,6 +12,10 @@ import networkx as nx
 from mlcroissant._src.core.issues import Issues
 from mlcroissant._src.core.rdf import Rdf
 
+_CONFORMS_TO_ERROR = (
+    "At least one of the provided conformsTo should be a valid CroissantVersion."
+)
+
 
 class CroissantVersion(enum.Enum):
     """Major and minor versions of the Croissant standard."""
@@ -30,14 +34,20 @@ class CroissantVersion(enum.Enum):
         elif not jsonld:
             return CroissantVersion.V_0_8
         else:
-            try:
-                return CroissantVersion(jsonld)
-            except ValueError:
-                ctx.issues.add_error(
-                    "conformsTo should be a string or a CroissantVersion. Got:"
-                    f" {jsonld}"
-                )
+            if isinstance(jsonld, list):
+                for conforms_to in jsonld:
+                    try:
+                        return CroissantVersion(conforms_to)
+                    except ValueError:
+                        pass
+                ctx.issues.add_error(_CONFORMS_TO_ERROR + f" Got: {jsonld}")
                 return CroissantVersion.V_0_8
+            else:
+                try:
+                    return CroissantVersion(jsonld)
+                except ValueError:
+                    ctx.issues.add_error(_CONFORMS_TO_ERROR + f" Got: {jsonld}")
+                    return CroissantVersion.V_0_8
 
     def to_json(self) -> str | None:
         """Serializes back to JSON-LD."""

python/mlcroissant/mlcroissant/_src/core/context_test.py
@@ -1,0 +1,55 @@
+"""context_test module."""
+
+import pytest
+
+from mlcroissant._src.core.context import Context
+from mlcroissant._src.core.context import CroissantVersion
+
+
+@pytest.mark.parametrize(
+    ["jsondl_conforms_to", "expected_conforms_to"],
+    [
+        [
+            ["http://mlcommons.org/croissant/1.0", "http://myorg/coolspec/0.1b"],
+            CroissantVersion.V_1_0,
+        ],
+        [
+            ["http://myorg/coolspec/0.1b", "http://mlcommons.org/croissant/1.0"],
+            CroissantVersion.V_1_0,
+        ],
+        [
+            "http://mlcommons.org/croissant/1.0",
+            CroissantVersion.V_1_0,
+        ],
+    ],
+)
+def test_croissant_version_from_jsonld(jsondl_conforms_to, expected_conforms_to):
+    ctx = Context()
+    ctx.conforms_to = CroissantVersion.from_jsonld(ctx, jsondl_conforms_to)
+    assert ctx.conforms_to == expected_conforms_to
+
+
+@pytest.mark.parametrize(
+    ["jsondl_conforms_to", "error_msg"],
+    [
+        [
+            123,
+            (
+                "At least one of the provided conformsTo should be a valid"
+                " CroissantVersion. Got: 123"
+            ),
+        ],
+        [
+            ["http://myorg/coolspec/0.1b", "http://myorg/coolspec/0.1c"],
+            (
+                "At least one of the provided conformsTo should be a valid"
+                " CroissantVersion. Got: ['http://myorg/coolspec/0.1b',"
+                " 'http://myorg/coolspec/0.1c']"
+            ),
+        ],
+    ],
+)
+def test_invalid_croissant_version_from_jsonld(jsondl_conforms_to, error_msg):
+    ctx = Context()
+    ctx.conforms_to = CroissantVersion.from_jsonld(ctx, jsondl_conforms_to)
+    assert error_msg in ctx.issues.errors

python/mlcroissant/mlcroissant/_src/structure_graph/nodes/croissant_version_test.py
@@ -13,7 +13,10 @@ from mlcroissant._src.core.context import CroissantVersion
 def test_conforms_to_is_invalid(conforms_to):
     ctx = Context(conforms_to=conforms_to)
     assert any(
-        error.startswith("conformsTo should be a string or a CroissantVersion.")
+        error.startswith(
+            "At least one of the provided conformsTo should be a valid"
+            " CroissantVersion."
+        )
         for error in ctx.issues.errors
     )
 

commit e401747aa4ec3e1ce8f47be48530357ed5029f32

Fix conformsTo (#898)
When no conformsTo is specified (as for example when creating metadata
from a notebook) then use the conformsTo from Context to populate that
field.

This also fixes the notebook errors introduced in
https://github.com/mlcommons/croissant/pull/892

python/mlcroissant/mlcroissant/_src/structure_graph/nodes/metadata.py
@@ -366,12 +366,13 @@ class Metadata(Node):
         jsonld = super().to_json()
         jsonld.pop("@id", None)
         jsonld["@context"] = context
+        conforms_to: str | list[str] | None = None
         if self.conforms_to:
             conforms_to = (
                 self.conforms_to[0] if len(self.conforms_to) == 1 else self.conforms_to
             )
-        else:
-            conforms_to = None
+        elif self.ctx.conforms_to:
+            conforms_to = self.ctx.conforms_to.to_json()
         jsonld["conformsTo"] = conforms_to
         if self.ctx.is_live_dataset:
             jsonld["isLiveDataset"] = self.ctx.is_live_dataset